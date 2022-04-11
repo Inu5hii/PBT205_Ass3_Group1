8941ebe77927fb04145cead1f9fccd003f48fc11
nabila-nazzy <nazarnabila@yahoo.com>
2022-04-11 16:34:00 +1000

    Added Nabila's Stock.h and Stocks.cpp folders

--- Stocks.cpp
+++ Stocks.cpp
@@ -1,1 +1,17 @@
 #include "Stocks.h"
+
+Stocks::Stocks()
+{
+}
+
+Stocks::Stocks(string username, int buyer, int seller, double price)
+{
+}
+
+void Stocks::buying(string username, int buyer, double price)
+{
+}
+
+void Stocks::selling(string username, int seller, double price)
+{
+}
--- Stocks.h
+++ Stocks.h
@@ -8,6 +8,18 @@ using namespace textpixels;
 
 class Stocks
 {
+private:
+	string username;
+	int buyer;
+	int seller;
+	int Quantity = 100; // quantity is a fixed number 
+	double price; // input byt the buy or seller
 
+public:
+	Stocks();
+	Stocks(string username, int buyer, int seller, double price);
+
+	void buying(string username, int buyer, double price);
+	void selling(string username, int seller, double price);
 };
 
